9413e0b634e1705036d6ad2def3d251a3fe36492
Sulley-Sadick <sulleysadick766@gmail.com>
2026-01-10 13:23:15 +0000

    Created Header.jsx, MainContent.jsx and Footer.jsx files and added their respective content within to create a simple UI && git push

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,17 @@
+/////////////////////////////// IMPORTS ////////////////////////
 import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage.jsx";
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <div>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = function () {
+  return (
+    <footer>
+      <p>&copy; 2025 City Lovers.</p>
+    </footer>
+  );
+};
+
+export default Footer;
