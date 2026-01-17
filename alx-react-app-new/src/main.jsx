12e3983e17448437a88f87d9b73b84637f541d0f
Sulley-Sadick <sulleysadick766@gmail.com>
2026-01-17 06:00:25 +0000

    Changed the destination of Counter call from the Main.jsx to App.jsx

--- alx-react-app-new/src/main.jsx
+++ alx-react-app-new/src/main.jsx
@@ -7,11 +7,9 @@ import "./index.css";
 
 // import components
 import App from "./App.jsx";
-import { Counter } from "./components/Counter.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <Counter />
   </StrictMode>,
 );
