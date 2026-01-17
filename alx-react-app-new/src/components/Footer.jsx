837e059357275ace20ec4a31d6721e443f4841e0
Sulley-Sadick <sulleysadick766@gmail.com>
2026-01-17 05:15:45 +0000

    Styled header.jsx, userProfile.jsx, MainContent.jsx, and Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = function () {
+  return (
+    <footer>
+      <p>
+        &copy; 2023 <span style={{ color: "pink" }}>City Lovers</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+const MainContent = function () {
+  return (
+    <main style={{ backgroundColor: "#afa5a81c" }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
+
+export default MainContent;
