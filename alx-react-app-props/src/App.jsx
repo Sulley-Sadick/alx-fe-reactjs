3c00308495bc84793acdffd2d5983a7fbdaca0a3
Sulley-Sadick <sulleysadick766@gmail.com>
2026-01-17 07:36:17 +0000

    Created alx-react-app-props and prevented props drilling by using the using the context api

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,29 @@
+///////////////////////////////// IMPORTS ///////////////////////////////
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage.jsx";
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
+import Footer from "./components/Footer.jsx";
+import UserProfile from "./components/UserProfile.jsx";
+import ProfilePage from "./components/ProfilePage.jsx";
+import { UserContext } from "./components/UserContext.js";
+function App() {
+  const userData = {
+    name: "Jane Doe",
+    email: "janedoe.email.com",
+  };
+  return (
+    <div>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+      <UserProfile name={"Alice"} age={25} bio={"Loves hiking and photography."} />
+    </div>
+  );
+}
+
+export default App;
---
+++ alx-react-app-props/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+const WelcomeMessage = function () {
+  return (
+    <div>
+      <h1>Hello everyone, I am learning React at ALX!.</h1>
+      <p>I am learning about JSX!.</p>
+    </div>
+  );
+};
+
+export default WelcomeMessage;
