00a98e7f5690edc3ab7bc1441c3d7c0763499807
Sulley-Sadick <sulleysadick766@gmail.com>
2026-01-26 14:44:35 +0000

    Added navigation functionality

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,13 @@
 import useRecipeStore from "./recipeStore";
+import { Navigate } from "react-router-dom";
 
 const DeleteRecipeButton = ({ id }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = Navigate();
 
   const handleDelete = () => {
     deleteRecipe(id);
+    navigate("/recipes");
   };
 
   return (
