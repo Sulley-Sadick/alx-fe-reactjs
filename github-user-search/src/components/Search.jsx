0d11fb9afda9970ba30704b9e8f39d7704596f22
Sulley-Sadick <sulleysadick766@gmail.com>
2026-02-01 07:36:40 +0000

    Configured tailwindcss and prettier

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -20,14 +20,13 @@ const SearchBar = function () {
       // get return data from fetchUserData functional component
       setData(await fetchUserData(username));
 
-      // clear input field
-      setUsername("");
-
       setStatusMessage("");
-    } catch (err) {
-      console.error("handleSubmit error", err);
+    } catch {
       setStatusMessage("Looks like we cant find the user");
     }
+
+    // clear input field
+    setUsername("");
   };
 
   //   css styles
@@ -38,18 +37,51 @@ const SearchBar = function () {
     gap: "1rem",
   };
 
+  const buttonStyles = {
+    backgroundColor: "#6400ed",
+    border: "none",
+    padding: "0.5rem",
+    color: "white",
+    borderRadius: "0.3rem",
+    width: "11.5rem",
+    cursor: "pointer",
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit} style={formStyles}>
-        <input type="search" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter username ..." style={{ padding: "0.5rem" }} />
-        <button type="submit">Submit</button>
+        <input
+          type="search"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Enter username ..."
+          style={{ padding: "0.5rem" }}
+        />
+        <input
+          type="search"
+          placeholder="Location..."
+          style={{ padding: "0.5rem" }}
+        />
+        <input
+          type="search"
+          placeholder="Repositories"
+          style={{ padding: "0.5rem" }}
+        />
+
+        <button type="submit" style={buttonStyles}>
+          Submit
+        </button>
       </form>
-      <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
-        <p className="data_paragraph">{statusMessage}</p>
+      <div>
+        <p className="data_paragraph">{statusMessage} </p>
         {data && (
           <>
             <p>{data.login}</p>
-            <img src={data.avatar_url} alt={data.avatar_url} style={{ borderRadius: "0.5rem" }} />
+            <img
+              src={data.avatar_url}
+              alt={data.avatar_url}
+              style={{ borderRadius: "0.5rem" }}
+            />
             <a href={data.html_url} target="_blank">
               Github Link
             </a>
