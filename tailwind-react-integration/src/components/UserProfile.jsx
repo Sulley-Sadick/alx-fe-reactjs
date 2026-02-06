da6d551650e6b457fa0e6eb338be7cd2bda054fc
Sulley-Sadick <sulleysadick766@gmail.com>
2026-02-06 05:49:51 +0000

    added styles for hover effect

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 function UserProfile() {
   return (
-    <div className="user-profile mx-auto my-20 max-w-xs rounded-lg bg-gray-100 shadow-lg sm:p-4 md:max-w-sm md:p-8">
+    <div className="user-profile mx-auto my-20 max-w-xs rounded-lg bg-gray-100 shadow-lg hover:shadow-xl sm:p-4 md:max-w-sm md:p-8">
       <img
         src="https://as2.ftcdn.net/v2/jpg/10/91/01/27/1000_F_1091012713_TPfuvkjNYjX53QDLW5lpPhzI1FUBKapt.jpg"
         alt="User"
-        className="rounded-full object-cover sm:h-24 sm:w-24 md:h-36 md:w-36"
+        className="rounded-full object-cover transition-transform duration-300 ease-in-out hover:scale-110 sm:h-24 sm:w-24 md:h-36 md:w-36"
       />
-      <h1 className="my-4 text-blue-800 sm:text-lg md:text-xl">John Doe</h1>
+      <h1 className="my-4 text-blue-800 hover:text-blue-500 sm:text-lg md:text-xl">
+        John Doe
+      </h1>
       <p className="text-gray-600 sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
