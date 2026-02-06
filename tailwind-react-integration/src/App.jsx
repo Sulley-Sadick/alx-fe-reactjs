8eb20419ca9fe70a285634bab04f0252df57dbb6
Sulley-Sadick <sulleysadick766@gmail.com>
2026-02-06 04:05:59 +0000

    Initial commit

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <p className="flex items-center justify-center">I have got my ticket for the long way round.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
