33f3e7d63985c486530179526e281171148571b4
Sulley-Sadick <sulleysadick766@gmail.com>
2026-02-06 05:40:08 +0000

    Changed styling of UserProfile.jsx component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
   return (
-    <div className="user-profile mx-auto my-20 max-w-sm rounded-lg bg-gray-100 p-8 shadow-lg">
+    <div className="user-profile mx-auto my-20 max-w-xs rounded-lg bg-gray-100 shadow-lg sm:p-4 md:max-w-sm md:p-8">
       <img
         src="https://as2.ftcdn.net/v2/jpg/10/91/01/27/1000_F_1091012713_TPfuvkjNYjX53QDLW5lpPhzI1FUBKapt.jpg"
         alt="User"
-        className="h-36 w-36 rounded-full object-cover"
+        className="rounded-full object-cover sm:h-24 sm:w-24 md:h-36 md:w-36"
       />
-      <h1 className="my-4 text-xl text-blue-800">John Doe</h1>
-      <p className="text-base text-gray-600">
+      <h1 className="my-4 text-blue-800 sm:text-lg md:text-xl">John Doe</h1>
+      <p className="text-gray-600 sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
