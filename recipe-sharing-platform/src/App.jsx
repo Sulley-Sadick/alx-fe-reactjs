3d487a8c82df1e5778921f389c272cd8a75c6408
Sulley-Sadick <sulleysadick766@gmail.com>
2026-02-11 13:53:25 +0000

    Changed tailwindcss configuration from version 4 to 3 for compatilibility reasons

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="mt-4 flex items-center justify-center gap-2 text-black">
+      <div className="mt-4 flex items-center justify-center gap-2 text-blue-500 ">
         <p>RECIPE SHARING PLATFORM.</p>
       </div>
     </>
