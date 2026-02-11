7dfd126a454a05460b495957586dbb3572a4f3ce
Sulley-Sadick <sulleysadick766@gmail.com>
2026-02-11 13:37:38 +0000

    configured vite, tailwindcss and react

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="mt-4 flex items-center justify-center gap-2 text-black">
+        <p>RECIPE SHARING PLATFORM.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
