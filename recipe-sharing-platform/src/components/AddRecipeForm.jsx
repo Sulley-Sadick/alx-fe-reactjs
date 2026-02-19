49b28c34fcf6137c5a9f804ce03b8f3e52372aa4
Sulley-Sadick <sulleysadick766@gmail.com>
2026-02-19 06:16:00 +0200

    file

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -6,26 +6,25 @@ function AddRecipeForm(){
     const [title, setTitle] = useState("");
     const [ingredients, setIngredients] = useState('');
     const [preparationSteps, setPreparationSteps] = useState('');
-    const [error, setError] = useState('');
+    const [errors, setErrors] = useState('');
 
     const handleSubmit  = (event)=> {
         // prevent browser default behavior
         event.preventDefault();
 
         if(!title  || !ingredients  || !preparationSteps){
-            setError('Please fill out input fields!')
+            setErrors('Please fill out input fields!')
             return
         }
 
         const allIngredients = ingredients.split('\n').filter(ing=> ing.trim() !== '');
 
         if(allIngredients.length < 2){
-            setError("Ingredients cannot be less than two items.")            
+            setErrors("Ingredients cannot be less than two items.")            
             return
         }
 
-
-        setError('')        
+        setErrors('')        
         
     }
     
@@ -33,7 +32,7 @@ function AddRecipeForm(){
     return (
         <form  onSubmit={handleSubmit} className="flex items-center flex-col gap-2">
 
-            {error && <p className="text-red-500">{error}</p>}
+            {errors && <p className="text-red-500">{errors}</p>}
 
             <label htmlFor="title">Title:</label>
             <input type="text" id="title" onChange={(e)=> setTitle(e.target.value)} className="input"/>
