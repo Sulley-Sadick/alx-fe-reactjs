00a317e55ced907a18c6da122138c451cf95e423
Sulley-Sadick <sulleysadick766@gmail.com>
2026-02-22 15:53:50 +0200

    changed state from formData to their respective names such as username, email, and password and used them at their respective input fields and retrieve state from input fields

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,26 +1,18 @@
 import { useState } from "react";
 
 function RegistrationForm() {
-  const [formData, setFormData] = useState({ username: "", email: "", password: "" });
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [errors, setErrors] = useState("");
-
-  const handleChange = function (e) {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
 
   const handleSubmit = function (e) {
     e.preventDefault();
 
-    if (!formData.username && !formData.email && !formData.password) {
+    if (!username && !email && !password) {
       setErrors("Please fill out this field");
       return;
     }
-
-    console.log(formData);
 
     setErrors("");
   };
@@ -28,13 +20,13 @@ function RegistrationForm() {
     <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       {errors && <p>{errors}</p>}
       <label htmlFor="username">Username:</label>
-      <input type="text" name="username" id="username" value={formData.username} onChange={handleChange} style={{ marginBlock: "0.5rem" }} />
+      <input type="text" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} style={{ marginBlock: "0.5rem" }} />
 
       <label htmlFor="email">Email:</label>
-      <input type="email" name="email" id="email" value={formData.email} onChange={handleChange} style={{ marginBlock: "0.5rem" }} />
+      <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} style={{ marginBlock: "0.5rem" }} />
 
       <label htmlFor="password">Password:</label>
-      <input type="password" name="password" id="password" value={formData.password} onChange={handleChange} style={{ marginBlock: "0.5rem" }} />
+      <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ marginBlock: "0.5rem" }} />
 
       <button type="submit">Submit</button>
     </form>
