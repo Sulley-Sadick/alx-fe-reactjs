69b3822b9b667b57e6602035e7f7c64e5c4c23a7
Sulley-Sadick <sulleysadick766@gmail.com>
2026-02-23 18:17:12 +0200

    Modified both PostComponent.jsx and App.jsx file

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -3,26 +3,30 @@ import { useQuery } from "react-query";
 const fetchPost = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
 
+  if (!response.ok) return "Network response was not ok";
+
   return response.json();
 };
 
 function PostComponents() {
-  const { data, error, isLoading } = useQuery({ queryKey: ["fetchPost"], queryFn: fetchPost, staleTime: 1000 });
+  const { data, isLoading, isError, refetch, isFetching } = useQuery({ queryKey: ["fetchPosts"], queryFn: fetchPost, staleTime: 1000 * 60 });
 
-  console.log(isLoading);
+  console.log("errorMessge", isError);
 
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error loading the data</div>;
+  if (isError) return <div>Error loading the data</div>;
 
   return (
     <div style={{ padding: "1rem" }}>
+      {isFetching && <p>Refreshing...</p>}
+
       {data.slice(0, 10).map((item) => (
         <div key={item.id}>
           <h1>{item.title}</h1>
           <p>{item.body}</p>
         </div>
       ))}
-      <button type="submit" style={{ display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: "#6400ed", marginInline: "auto", width: "100%", border: "none", padding: "1rem", color: "white" }}>
+      <button type="submit" style={{ display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: "#6400ed", marginInline: "auto", width: "50%", border: "none", padding: "1rem", color: "white", borderRadius: "1rem", cursor: "pointer" }} onClick={refetch}>
         {" "}
         Refresh
       </button>
