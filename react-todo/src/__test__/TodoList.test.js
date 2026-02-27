737148d18f5036670c35816d813e8b425cdf8fec
Sulley-Sadick <sulleysadick766@gmail.com>
2026-02-27 18:15:33 +0200

    wrapped all test code around describe

---
+++ react-todo/src/__test__/TodoList.test.js
@@ -0,0 +1,55 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../components/TodoList";
+
+describe("Todolist", () => {
+  // TodoList component render correctly
+  test("should render TodoList component", () => {
+    render(<TodoList />);
+
+    expect(screen.getByText("Do 120 pushups")).toBeInTheDocument();
+    expect(screen.getByText("Read Think Again by Adam Grant")).toBeInTheDocument();
+  });
+
+  // new todo added test
+  test("should add a new todo", () => {
+    render(<TodoList />);
+
+    const input = screen.getByPlaceholderText("Add Todo ...");
+    const addButton = screen.getByText("Add");
+
+    fireEvent.change(input, { target: { value: "Make money" } });
+    fireEvent.click(addButton);
+
+    expect(screen.getByText("Make money")).toBeInTheDocument();
+  });
+
+  // todo marked as completed
+  test("should toggle a todo item completed", () => {
+    render(<TodoList />);
+
+    const todoItem = screen.getByText("Read Think Again by Adam Grant");
+
+    expect(todoItem).toHaveStyle("text-decoration: none");
+
+    fireEvent.click(todoItem);
+    expect(todoItem).toHaveStyle("text-decoration: line-through");
+
+    fireEvent.click(todoItem);
+    expect(todoItem).toHaveStyle("text-decoration: none");
+  });
+
+  // todo item deleted when button is being clicked
+  test("todo item can be deleted when button is clicked", () => {
+    render(<TodoList />);
+
+    const todoText = screen.getByText("Read Think Again by Adam Grant");
+
+    expect(todoText).toBeInTheDocument();
+
+    const deleteButton = todoText.closest("li").querySelector("button");
+
+    fireEvent.click(deleteButton);
+
+    expect(screen.queryByText("Read Think Again by Adam Grant")).not.toBeInTheDocument();
+  });
+});
